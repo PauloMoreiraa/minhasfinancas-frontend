5e2a4fc77df309cf9cbe022c5277ffab8117229e
Paulo Moreira <nagatogts@hotmail.com>
2024-09-06 12:58:39 -0300

    feat: Salvando lançamenots com o serviço da API de lançamentos + atualização de rotas

--- src/views/lancamentos/cadastro-lancamentos.js
+++ src/views/lancamentos/cadastro-lancamentos.js
@@ -1,17 +1,59 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import * as messages from "../../components/toastr"
 
 import Card from "../../components/card";
 import FormGroup from "../../components/form-group";
 import SelectMenu from "../../components/selectMenu"
 
 import LancamentoService from "../../app/service/lancamentoService"
+import LocalStorageService from "../../app/service/localstorageService"
 
 class CadastroLancamentos extends React.Component{
+
+    state = {
+        id: null,
+        descricao: '',
+        valor: '',
+        mes: '',
+        ano: '',
+        tipo: '',
+        status: ''
+    }
 
     constructor(){
         super();
         this.service = new LancamentoService();
+    }
+
+    submit = () => {
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado')
+
+        const {descricao, valor, mes, ano, tipo} = this.state;
+
+        const lancamento = {
+            descricao: descricao,
+            valor: valor,
+            mes: mes,
+            ano: ano,
+            tipo: tipo, 
+            usuario: usuarioLogado.id
+        }
+
+        this.service
+            .salvar(lancamento)
+            .then(response => {
+                this.props.history.push('/consulta-lancamentos')
+                messages.mensagemSucesso('Lançamento cadastrado com sucesso!')
+            }).catch(error =>{
+                messages.mensagemErro(error.response.data)
+            })
+    }
+
+    handleChange = (event) => {
+        const value = event.target.value
+        const name = event.target.name
+        this.setState({ [name] : value })
     }
 
     render(){
@@ -23,43 +65,44 @@ class CadastroLancamentos extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <FormGroup id="inputDescricao" label="Descrição: *">
-                            <input id="inputDescricao" type="text" className="form-control" />
+                            <input id="inputDescricao" type="text" className="form-control" name="descricao" value={this.state.descricao} onChange={this.handleChange} />
                         </FormGroup>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
                         <FormGroup id="inputAno" label="Ano: *">
-                            <input id="inputAno" type="text" className="form-control" />
+                            <input id="inputAno" type="text" className="form-control" name="ano" value={this.state.ano} onChange={this.handleChange} />
                         </FormGroup>
                     </div>
                     <div className="col-md-6">
                         <FormGroup id="inputMes" label="Mês: *">
-                            <SelectMenu className="form-control" id="inputMes" lista={meses} />
+                            <SelectMenu className="form-control" id="inputMes" lista={meses} name="mes" value={this.state.mes} onChange={this.handleChange} />
                         </FormGroup>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-4">
                         <FormGroup id="inputValor" label="Valor: *">
-                            <input id="inputValor" type="text" className="form-control" />
+                            <input id="inputValor" type="text" className="form-control" name="valor" value={this.state.valor} onChange={this.handleChange} />
                         </FormGroup>
                     </div>
                     <div className="col-md-4">
                         <FormGroup id="inputTipo" label="Tipo: *">
-                           <SelectMenu className="form-control" id="inputTipo" lista={tipos} />
+                           <SelectMenu className="form-control" id="inputTipo" lista={tipos} name="tipo" value={this.state.tipo} onChange={this.handleChange} />
                         </FormGroup>
                     </div>
                     <div className="col-md-4">
                         <FormGroup id="inputStatus" label="Status: ">
-                           <input type="text" className="form-control" disabled/>
+                           <input type="text" className="form-control" name="status" value={this.state.status} onChange={this.handleChange} disabled/>
                         </FormGroup>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
-                        <button className="btn btn-success">Salvar</button>
-                        <button className="btn btn-danger">Cancelar</button>
+                        <button onClick={this.submit} className="btn btn-success">Salvar</button>
+                        <button onClick={e =>
+                this.props.history.push('/consulta-lancamentos')} className="btn btn-danger">Cancelar</button>
                     </div>
                 </div>
             </Card>
--- src/views/lancamentos/consulta-lancamentos.js
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -81,6 +81,10 @@ class ConsultaLancamentos extends React.Component{
             })
     }
 
+    preparaFormularioCadastro = () => {
+        this.props.history.push('/cadastro-lancamentos')
+    }
+
     render(){
         const meses = this.service.obterListaMeses();
         const tipos = this.service.obterListaTipos();
@@ -111,7 +115,7 @@ class ConsultaLancamentos extends React.Component{
                                 <SelectMenu onChange={e => this.setState({tipo: e.target.value})} value={this.state.tipo} id="inputTipo" className="form-control" lista={tipos} />
                             </FormGroup>
                             <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
-                            <button type="button" className="btn btn-danger">Cadastrar</button>
+                            <button onClick={this.preparaFormularioCadastro} type="button" className="btn btn-danger">Cadastrar</button>
                         </div>
                     </div>
                 </div>
